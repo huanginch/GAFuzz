d019795d5bafebc6d3fd137d23d08fa814ab776c
huanginch <huanginch329@gmail.com>
2024-03-22 12:10:13 +0800

    @update: change axios to got and use the timing in got

--- src/fuzzReq.js
+++ src/fuzzReq.js
@@ -1,38 +1,17 @@
-import axios from 'axios';
-import rateLimit from 'axios-rate-limit';
+import got from 'got';
 
-async function fuzzReq(url, data) {
+const fuzzReq = async (url, data) => {
   try {
-    let resTime = 0;
-    const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 10000 });
-    http.interceptors.request.use(x => {
-      // to avoid overwriting if another interceptor
-      // already defined the same object (meta)
-      x.meta = x.meta || {};
-      x.meta.requestStartedAt = new Date().getTime();
-      return x;
-    })
-
-    http.interceptors.response.use(x => {
-      resTime = new Date().getTime() - x.config.meta.requestStartedAt;
-      // console.log(x.config.meta.requestStartedAt);
-      // console.log(resTime);
-      return x;
-    },
-      // Handle 4xx & 5xx responses
-      x => {
-        resTime = new Date().getTime() - x.config.meta.requestStartedAt;
-        throw x;
-      }
-    )
-    http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    const res = await http.post(url, data);
-    const statusCode = res.status;
+    const response = await got.get(url, { searchParams: data });
+    const resTime = response.timings.phases.firstByte;
+    const statusCode = response.statusCode;
     return { resTime, statusCode };
   }
   catch (err) {
-    const statusCode = err.code;
-    return { resTime: 0, statusCode };
+    // console.log(err);
+    const resTime = err.timings? err.timings.phases.firstByte : 0;
+    const statusCode = err.response? err.response.statusCode + ' ' + err.response.statusMessage : err.code;
+    return { resTime, statusCode };;
   }
 }
 
